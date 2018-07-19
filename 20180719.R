9bd7b672ee74b8f1c436c78750ac9ef47c52c1cc
dorgon <dorgon.chang@mai.ai>
2018-07-19 18:58:22 +0800

    test

---
+++ 20180719.R
@@ -0,0 +1,15 @@
+# 20180719
+# ctrl + enter => execute
+
+1 + 3
+
+x <- sum(1:10)
+
+cat("Hello R users! \n")
+cat("3 + 5 = ", 3 + 5, "\n")
+
+head(iris)
+tail(iris)
+View(iris)
+
+plot(iris[,1], iris[,2], col=iris[,5])
